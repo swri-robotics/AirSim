16f0ab2e546fac021cdc013f2c9182105a674a5d
P. J. Reed <preed@swri.org>
2020-04-23 08:22:56 -0500

    Clean out environments we don't care about
    Former-commit-id: f10f6845019cfcded77a78deb564a0a82b65d95a

--- build.sh
+++ build.sh
@@ -83,9 +83,9 @@ rsync -a --delete MavLinkCom/include AirLib/deps/MavLinkCom
 rsync -a --delete AirLib Unreal/Plugins/AirSim/Source
 
 # Update Blocks project
-Unreal/Environments/Blocks/clean.sh
-mkdir -p Unreal/Environments/Blocks/Plugins
-rsync -a --delete Unreal/Plugins/AirSim Unreal/Environments/Blocks/Plugins
+# Unreal/Environments/Blocks/clean.sh
+# mkdir -p Unreal/Environments/Blocks/Plugins
+# rsync -a --delete Unreal/Plugins/AirSim Unreal/Environments/Blocks/Plugins
 
 set +x
 
